a6320414c8c89aaca035cde2c1ad0616fdab02b5
Eyal Brilling <eyalbrilling@gmail.com>
2023-05-23 16:25:22 +0300

    - Starting of new represntion. - bug with indices row 29

--- ex1.py
+++ ex1.py
@@ -2,6 +2,8 @@ import search
 import random
 import math
 
+_debug_ = True
+
 #Please write your IDs here.
 ids = ["111111111", "111111111"]
 
@@ -10,10 +12,36 @@ class TaxiProblem(search.Problem):
     """This class implements a medical problem according to problem description file"""
 
     def __init__(self, initial):
-        """Don't forget to implement the goal test
-        You should change the initial to your own representation.
-        search.Problem.__init__(self, initial) creates the root node"""
-        search.Problem.__init__(self, initial)
+
+        original_problem = initial.copy()
+        new_rep = initial.copy()
+        
+        # Add gas station map to new_rep
+        for row,value in enumerate(original_problem["map"]):
+            for col,value2 in enumerate(original_problem["map"][row]):
+                if value == 'G':
+                    new_rep["gas_stations"] |= (row,col)
+
+
+        # Insert into map the taxis themself. value in map = index of taxi in map
+        for index,taxi in enumerate(original_problem["taxis"]):
+            print(taxi)
+            taxi_location = taxi["location"]
+            # In the taxi location, Put the taxi index.
+            new_rep["map"][taxi_location[0]][taxi_location[1]] = str(index)
+
+        # Add Max fuel to taxis in new_rep
+        for index,taxi in enumerate(original_problem["taxis"]):
+            new_rep["taxis"][index]["max_fuel"] = taxi["fuel"]
+
+        # Add empty passengers list to taxis in new_rep. passengers will be by index
+        for index,taxi in enumerate(original_problem["taxis"]):
+            new_rep["taxis"][index]["passengers"] = []
+
+        if _debug_:
+            print("new_rep: ", new_rep)
+
+        search.Problem.__init__(self, new_rep)
         
     def actions(self, state):
         """Returns all the actions that can be executed in the given
